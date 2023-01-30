6b5f3142cec0dfcc13d324f1d5811d6dc4894d04
grldt <geraldstephanus@gmail.com>
2023-01-30 16:57:12 +0700

    Initial Commit

---
+++ getplus test/getplus_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  getplus_testApp.swift
+//  getplus test
+//
+//  Created by Gerald Stephanus on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct getplus_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
